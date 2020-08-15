20382d817d17e13eb10a233b94fdd0bea7d1ea15
AnansiOmega <ignasbutautas7@gmail.com>
2020-08-15 15:17:54 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
 class Actor < ActiveRecord::Base
-  
+has_many :characters
+has_many :shows, through: :characters
+
+    def full_name
+        name = []
+        name << self.first_name
+        name << self.last_name
+        name.join(" ")
+    end
+
+    def list_roles
+        # shows = self.shows.map(&:name)
+        # characters = self.characters.map(&:name)
+        # x = Hash[shows.zip(characters)]
+        # x.map {|k,v| "#{k} - #{v}"}.flatten.each_slice(2).to_a
+        "#{self.characters.first.name} - #{self.shows.first.name}"
+    end
+
 end
